b89f5ba0bd045b722873ff355e1c6370905d9ee8
juyeon <misssim29@naver.com>
2024-01-19 13:41:19 +0900

    expo버전 분리

--- module-test/app/_layout.tsx
+++ module-test/app/_layout.tsx
@@ -2,14 +2,11 @@ import { Slot, router } from "expo-router";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import OneSignal from "../components/OneSignal";
-import Constants, { ExecutionEnvironment } from "expo-constants";
 const Layout = () => {
-  const isExpoGo =
-    Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
   return (
     <View>
-      {!isExpoGo ? <OneSignal /> : ""}
       <SafeAreaView>
+        <OneSignal />
         <Slot />
       </SafeAreaView>
     </View>
--- module-test/components/OneSignal.tsx
+++ module-test/components/OneSignal.tsx
@@ -1,18 +1,33 @@
-import { OneSignal } from "react-native-onesignal";
-import { useState } from "react";
 import { router } from "expo-router";
+import { View, Text } from "react-native";
+import Constants, { ExecutionEnvironment } from "expo-constants";
+const isExpoGo =
+  Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
+
+let OneSignal: any;
+
+if (!isExpoGo) {
+  OneSignal = require("react-native-onesignal").OneSignal;
+} else {
+  OneSignal = View;
+}
 
 const OneSignalComponent = () => {
-  OneSignal.initialize("c87ff605-e618-421e-bc28-bbff082c3b78");
-  OneSignal.Notifications.requestPermission(true);
-  //밖에서 클릭
-  OneSignal.Notifications.addEventListener("click", (event) => {
-    router.replace(`/article/${event.notification.rawPayload?.custom.a.id}`);
-  });
-  //인앱일때
-  OneSignal.Notifications.addEventListener("foregroundWillDisplay", (event) => {
-    router.replace(`/article/${event.notification.rawPayload?.custom.a.id}`);
-  });
+  if (!isExpoGo) {
+    OneSignal.initialize("c87ff605-e618-421e-bc28-bbff082c3b78");
+    OneSignal.Notifications.requestPermission(true);
+    //밖에서 클릭
+    OneSignal.Notifications.addEventListener("click", (event) => {
+      router.replace(`/article/${event.notification.additionalData?.id}`);
+    });
+    //인앱일때
+    OneSignal.Notifications.addEventListener(
+      "foregroundWillDisplay",
+      (event) => {
+        router.replace(`/article/${event.notification.additionalData?.id}`);
+      }
+    );
+  }
   return <></>;
 };
 
